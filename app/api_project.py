e9af4ceb74c319c2acc01db1876afc9c51545dfe
jessegabriels <r0882112@student.thomasmore.be>
2022-11-20 00:30:17 +0100

    php

--- app/api_project.py
+++ app/api_project.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 #import mysql.connector
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 meetinglist = [[1,14.92], [2, 25.45], [3, 18.26], [4, 18.72], [5, 19.11], [6, 19.02], [7, 38.85], [8, 39.78]]
@@ -21,6 +20,7 @@ origins = [
     "http://localhost:8080",
     "https://localhost.tiangolo.com",
     "http://127.0.0.1:5500"
+    "http://localhost:63342"
 ]
 
 app.add_middleware(
@@ -30,7 +30,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/meeting")
 def root():
